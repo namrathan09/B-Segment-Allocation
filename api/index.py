02740a144a3cee80764e8e8385018925b4db4901
Namratha N <148744423+namrathan09@users.noreply.github.com>
2025-12-19 14:35:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -352,20 +352,25 @@ def process_central_file_step3_final_merge_and_needs_review(consolidated_df, upd
  
         if channel == 'PISA' and not df_pisa_indexed.empty and barcode in df_pisa_indexed.index:
             pisa_row = df_pisa_indexed.loc[barcode]
-            vendor_name = pisa_row.get('vendor') if pisa_row.get('vendor') else vendor_name
+            vendor_name = pisa_row.get('vendor_name') if pisa_row.get('vendor_name') else vendor_name
             vendor_number = pisa_row.get('vendor_number') if pisa_row.get('vendor_number') else vendor_number
             company_code = pisa_row.get('company_code') if pisa_row.get('company_code') else company_code
+            received_date = pisa_row.get('received_date') if pd.notna(pisa_row.get('received_date')) else received_date
            
         elif channel == 'ESM' and not df_esm_indexed.empty and barcode in df_esm_indexed.index:
             esm_row = df_esm_indexed.loc[barcode] # Accessing by 'barcode' index
             company_code = esm_row.get('company_code') if esm_row.get('company_code') else company_code
             category = esm_row.get('subcategory') if esm_row.get('subcategory') else category
+            vendor_name = esm_row.get('vendor_name') if esm_row.get('vendor_name') else vendor_name
+            vendor_number = esm_row.get('vendor_number') if esm_row.get('vendor_number') else vendor_number
+            received_date = esm_row.get('received_date') if pd.notna(esm_row.get('received_date')) else received_date
  
         elif channel == 'PM7' and not df_pm7_indexed.empty and barcode in df_pm7_indexed.index:
             pm7_row = df_pm7_indexed.loc[barcode]
             vendor_name = pm7_row.get('vendor_name') if pm7_row.get('vendor_name') else vendor_name
             vendor_number = pm7_row.get('vendor_number') if pm7_row.get('vendor_number') else vendor_number
             company_code = pm7_row.get('company_code') if pm7_row.get('company_code') else company_code
+            received_date = pm7_row.get('received_date') if pd.notna(pm7_row.get('received_date')) else received_date
  
         new_central_row_data = row_consolidated.to_dict()
         new_central_row_data['Vendor Name'] = vendor_name if vendor_name is not None else ''
